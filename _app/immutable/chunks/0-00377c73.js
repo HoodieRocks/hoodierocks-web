30412b6f5bc40d4506955c6cc65707015d7670ec
1ndiigo <aidenhperry@gmail.com>
2022-11-13 17:27:56 -0500

    Updates

--- _app/immutable/chunks/0-00377c73.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-0b9d9551.js";export{t as component};
--- _app/immutable/chunks/1-0e091ed1.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-34cf6abd.js";export{t as component};
