e141b1d81dc99a505de1e7281cba833ecf4c7dd2
1ndiigo <aidenhperry@gmail.com>
2022-11-13 17:31:50 -0500

    Updates

--- _app/immutable/chunks/0-1ca16a9c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-c65fe1af.js";export{t as component};
---
+++ _app/immutable/chunks/1-2b06df85.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-875557a0.js";export{t as component};
