043a7fe8dd9ea788d2a18373f6a37416a902ed49
HoodieRocks <aidenhperry@gmail.com>
2022-11-14 16:52:38 -0500

    Updates

---
+++ _app/immutable/chunks/2-b069e121.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-dbc82415.js";export{t as component};
--- _app/immutable/chunks/2-fa210c5e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-425c9964.js";export{t as component};
